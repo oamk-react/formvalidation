2740bb3cff1d25819ce0c302340051deabcbc9d9
Jouni Juntunen <jouni.juntunen@oamk.fi>
2020-11-12 08:41:52 +0200

    Finished example.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
+// npm install formik --save
+import { useFormik } from 'formik';
+// npm install yup --save
+import * as Yup from "yup";
+
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      full_name: "",
+      email: "",
+      password: "",
+      confirm_password: ""
+    },
+    validationSchema: Yup.object({
+      full_name: Yup.string()
+        .min(2, "Mininum 2 characters")
+        .max(15, "Maximum 15 characters")
+        .required("Required!"),
+      email: Yup.string()
+        .email("Invalid email format")
+        .required("Required!"),
+      password: Yup.string()
+        .min(8, "Minimum 8 characters")
+        .required("Required!"),
+      confirm_password: Yup.string()
+        .oneOf([Yup.ref("password")], "Password's not match")
+        .required("Required!")
+    }),
+    onSubmit: values => {
+      console.log('onSubmit');
+      alert(JSON.stringify(values, null,2));
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Validation with Formik + Yup</h1>
+
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <label>Full Name</label>
+          <input
+            type="text"
+            name="full_name"
+            value={formik.values.full_name}
+            onChange={formik.handleChange}
+          />
+          {formik.errors.full_name && formik.touched.full_name && (
+            <p>{formik.errors.full_name}</p>
+          )}
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+          />
+          {formik.errors.email && formik.touched.email && (
+            <p>{formik.errors.email}</p>
+          )}
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+          />
+          {formik.errors.password && formik.touched.password && (
+            <p>{formik.errors.password}</p>
+          )}
+        </div>
+        <div>
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            name="confirm_password"
+            value={formik.values.confirm_password}
+            onChange={formik.handleChange}
+          />
+          {formik.errors.confirm_password && formik.touched.confirm_password && (
+            <p>{formik.errors.confirm_password}</p>
+          )}
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
